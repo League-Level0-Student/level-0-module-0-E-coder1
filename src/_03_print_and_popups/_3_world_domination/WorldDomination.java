92e4d973a2be6d811a9c6cf59c9dfca8bc5e9f22
league <league@imac-24.attlocal.net>
2023-02-25 11:24:59 -0800

    Quiz Game Finished

--- src/_03_print_and_popups/_3_world_domination/WorldDomination.java
+++ src/_03_print_and_popups/_3_world_domination/WorldDomination.java
@@ -8,9 +8,14 @@ public class WorldDomination {
 	public static void main(String[] args) {
 		
 		// 1. Ask the user if they know how to write code.
-
+		String asking = JOptionPane.showInputDialog("Do you know how to write code?");
+		
 		// 2. If they say "yes", tell them they will rule the world.
-
+		if (asking.equals("yes")) {
+			System.out.println("You rule the world!");
+		} else { 	
+			System.out.println("Sign up for classes at The League");
+		}
 		// 3. Otherwise, tell them to sign up for classes at The League.
 
 	}
--- src/_03_print_and_popups/_4_quiz_game/QuizGame.java
+++ src/_03_print_and_popups/_4_quiz_game/QuizGame.java
@@ -1,21 +1,27 @@
 package _03_print_and_popups._4_quiz_game;
 
+import javax.swing.JOptionPane;
+
 public class QuizGame {
 	public static void main(String[] args) {
 		
 		// Create a variable to hold the user's score. Set it equal to zero. 
-		
+		int UserScore = 0;
 		// ASK A QUESTION AND CHECK THE ANSWER
 		
 				// 2.  Ask the user a question 
-				
+				String question = JOptionPane.showInputDialog("Do whales live in the ocean?");
 				// 3.  Use an if statement to check if their answer is correct
-				
+				if (question.equals("yes")) {
+					JOptionPane.showMessageDialog(null,UserScore + 1);
+				} else { 	
+					JOptionPane.showMessageDialog(null,UserScore - 1);
+				}
 				// 4.  if the user's answer was correct, add one to their score 
 		
 		// MAKE MORE QUESTIONS. Ask more questions by repeating the above 
 				// Option: Subtract a point from their score for a wrong answer
-		
+				
 		
 		// After all the questions have been asked, tell the user their final score 
 		
